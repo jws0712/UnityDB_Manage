99440b3d1a67da04ecabba7dc4a669e6c0bfe9d4
jws2170 <mpart2077@gmail.com>
2024-06-05 00:57:47 +0900

    [ADD] SlotClickSystem

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Item
@@ -17,7 +20,11 @@ public class Item
 public class GameManager : MonoBehaviour
 {
     public TextAsset ItemDatabase;
-    public List<Item> AllItemList;
+    public List<Item> AllItemList, MyItemList, CurItemList;
+    public string curType = "Character";
+    public GameObject[] Slot;
+    public Image[] TabImage;
+    public Sprite TabIdleSprite, TabSelectSprite;
     void Start()
     {
         string[] line = ItemDatabase.text.Substring(0, ItemDatabase.text.Length - 1).Split('\n');
@@ -27,5 +34,65 @@ public class GameManager : MonoBehaviour
 
             AllItemList.Add(new Item(row[0], row[1], row[2], row[3], row[4] == "TRUE"));
         }
+
+        Load();
+    }
+
+    public void SlotClick(int slotNum)
+    {
+        Item CurItem = CurItemList[slotNum];
+        Item UsingItem = CurItemList.Find(x => x.isUsing == true);
+
+        if(curType == "Character")
+        {
+            if(UsingItem != null) UsingItem.isUsing = false;
+            CurItem.isUsing = true;
+        }
+        else
+        {
+            CurItem.isUsing = !CurItem.isUsing;
+            if (UsingItem != null) UsingItem.isUsing = false; 
+        }
+
+        Save();
+    }
+
+    public void TabClick(string tabName)
+    {
+        curType = tabName;
+        CurItemList = MyItemList.FindAll(x => x.Type == tabName);
+
+        for(int i = 0;i < Slot.Length;i++)
+        {
+            Slot[i].SetActive(i < CurItemList.Count);
+            Slot[i].GetComponentInChildren<Text>().text = i < CurItemList.Count ? CurItemList[i].Name + "/" + CurItemList[i].isUsing : " ";
+        }
+
+        int tabNum = 0;
+        switch (tabName)
+        {
+            case "Character": tabNum = 0; break;
+            case "Balloon": tabNum = 1; break;
+        }
+        for(int i = 0; i < TabImage.Length ; i++)
+        {
+            TabImage[i].sprite = i == tabNum ? TabSelectSprite : TabIdleSprite;
+        }
+    }
+
+    void Save()
+    {
+        string jdata = JsonConvert.SerializeObject(MyItemList);
+        File.WriteAllText(Application.dataPath + "/Resources/MyItemText.txt", jdata);
+
+        TabClick(curType);
+    }
+
+    void Load()
+    {
+        string jdata = File.ReadAllText(Application.dataPath + "/Resources/MyItemText.txt");
+        MyItemList = JsonConvert.DeserializeObject<List<Item>>(jdata);
+
+        TabClick(curType);
     }
 }
